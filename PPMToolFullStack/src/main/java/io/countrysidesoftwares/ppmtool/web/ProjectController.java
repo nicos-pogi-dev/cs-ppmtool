20c7457239d86455bdbad333efcd55d7db04689a
nicos-pogi-dev <nicos.fopalan.upwork@gmai.com>
2021-01-22 15:08:09 +0800

    Project validation Part 1

--- PPMToolFullStack/src/main/java/io/countrysidesoftwares/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/countrysidesoftwares/ppmtool/web/ProjectController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,12 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project project){
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		
+		if(result.hasErrors()) {
+			return new ResponseEntity<String> ("Invalid Oject", HttpStatus.BAD_REQUEST);
+		}
+		
 		projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 		
